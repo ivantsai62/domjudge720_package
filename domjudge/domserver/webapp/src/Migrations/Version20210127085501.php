44161568e92c780e2edcc30bb781a715bc3a3be1
ivantsai62 <73215208+ivantsai62@users.noreply.github.com>
2021-02-09 05:31:17 +0000

    Update Version20210127085501.php
    migrate change

--- domjudge/domserver/webapp/src/Migrations/Version20210127085501.php
+++ domjudge/domserver/webapp/src/Migrations/Version20210127085501.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210127085501 extends AbstractMigration
+final class Version20210128071104 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,11 +22,12 @@ final class Version20210127085501 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE problem_group (id INT AUTO_INCREMENT NOT NULL, probid INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE problem CHANGE restriction_languages restriction_languages LONGTEXT DEFAULT \'NULL\' COMMENT \'JSON-encoded restrictions(DC2Type:json)\', CHANGE subtask subtask INT DEFAULT NULL, CHANGE task_point task_point LONGTEXT DEFAULT \'NULL\' COMMENT \'JSON-encoded restrictions(DC2Type:json)\', CHANGE problems_group problems_group LONGTEXT DEFAULT \'NULL\' COMMENT \'JSON-encoded restrictions(DC2Type:json)\'');
         $this->addSql('ALTER TABLE judging_run CHANGE task task INT DEFAULT NULL');
         $this->addSql('ALTER TABLE judging CHANGE task_result task_result LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
-        $this->addSql('ALTER TABLE scorecache CHANGE all_task_point all_task_point LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
         $this->addSql('ALTER TABLE testcase CHANGE orig_input_filename orig_input_filename VARCHAR(255) DEFAULT NULL COMMENT \'Original basename of the input file.\', CHANGE task task INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE scorecache CHANGE all_task_point all_task_point LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema) : void
@@ -34,6 +35,7 @@ final class Version20210127085501 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE problem_group');
         $this->addSql('ALTER TABLE judging CHANGE task_result task_result LONGTEXT DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:json)\'');
         $this->addSql('ALTER TABLE judging_run CHANGE task task INT DEFAULT NULL');
         $this->addSql('ALTER TABLE problem CHANGE restriction_languages restriction_languages LONGTEXT DEFAULT \'\'NULL\'\' COLLATE utf8mb4_unicode_ci COMMENT \'JSON-encoded restrictions(DC2Type:json)\', CHANGE subtask subtask INT DEFAULT NULL, CHANGE task_point task_point LONGTEXT DEFAULT \'\'NULL\'\' COLLATE utf8mb4_unicode_ci COMMENT \'JSON-encoded restrictions(DC2Type:json)\', CHANGE problems_group problems_group LONGTEXT DEFAULT \'\'NULL\'\' COLLATE utf8mb4_unicode_ci COMMENT \'JSON-encoded restrictions(DC2Type:json)\'');
